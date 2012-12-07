e4708299424edae3c672fa0c879383cc810499eb
Tse-Ching Ho <tsechingho@gmail.com>
2012-12-07 11:34:01 +0800

    be a rails engine

--- lib/rails-theme-helper.rb
+++ lib/rails-theme-helper.rb
@@ -1,4 +1,5 @@
 require 'rails-theme-helper/version'
+require 'rails-theme-helper/engine'
 
 module RailsThemeHelper
 end
---
+++ lib/rails-theme-helper/engine.rb
@@ -0,0 +1,4 @@
+module RailsTheme
+  class Engine < ::Rails::Engine
+  end
+end

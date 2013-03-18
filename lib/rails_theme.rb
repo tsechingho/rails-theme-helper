b4c7bf6d639b9a1a493d80c20a938ab372fb74ac
Tse-Ching Ho <tsechingho@gmail.com>
2013-03-18 19:04:14 +0800

    move require statement to engine

--- lib/rails_theme.rb
+++ lib/rails_theme.rb
@@ -1,11 +1,4 @@
-require 'rails-i18n'
-
 require 'rails_theme/engine'
-
-if defined? ::WillPaginate
-  require 'will_paginate/array'
-  require 'rails_theme/bootstrap/link_renderer'
-end
 
 module RailsTheme
 end
--- lib/rails_theme/engine.rb
+++ lib/rails_theme/engine.rb
@@ -2,3 +2,10 @@ module RailsTheme
   class Engine < ::Rails::Engine
   end
 end
+
+require 'rails-i18n'
+
+if defined? ::WillPaginate
+  require 'will_paginate/array'
+  require 'rails_theme/bootstrap/link_renderer'
+end

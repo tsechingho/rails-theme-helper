2ea5c7d1f4655b0d2f353688bb999e2330bcce6f
Tse-Ching Ho <tsechingho@gmail.com>
2013-11-12 16:05:47 +0800

    rescue current_page? in render_list

--- app/helpers/rails_theme/layout_helper.rb
+++ app/helpers/rails_theme/layout_helper.rb
@@ -17,7 +17,7 @@ module RailsTheme
         item_class << 'first' if index == 0
         item_class << 'last' if index == (list.length - 1)
         link = item_content.match(/href=(["'])(.*?)(\1)/)[2] rescue nil
-        item_class << 'current active' if link and current_page?(link)
+        item_class << 'current active' if link and (current_page?(link) rescue false)
         item_class << item_options.delete(:class) if item_options[:class]
 
         li_class = item_class.empty? ? nil : item_class.join(' ')
